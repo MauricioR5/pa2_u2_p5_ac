6be9b5b6f73313c58dcb0f95acfc506e72f898a9
Mauricio Cacuango <amcacuangoa@uce.edu.ec>
2023-12-11 19:47:01 -0500

    Taller 14

---
+++ src/main/java/com/uce/edu/avanzada/Pa2U2P5AcApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.avanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U2P5AcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U2P5AcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/edu/avanzada/Pa2U2P5AcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.avanzada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa2U2P5AcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
