dc09e50e01cc19ff90d7b36c415990ea12cb367a
Alexander Cacuango <amcacuangoa@uce.edu.ec>
2023-12-15 12:49:06 -0500

    Taller_15

--- src/main/java/com/uce/edu/Pa2U2P5AcApplication.java
+++ src/main/java/com/uce/edu/Pa2U2P5AcApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Pa2U2P5AsApplication implements CommandLineRunner {
+public class Pa2U2P5AcApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Pa2U2P5AsApplication.class, args);
+		SpringApplication.run(Pa2U2P5AcApplication.class, args);
 	}
 
 	@Override
--- src/test/java/com/uce/edu/Pa2U2P5AcApplicationTests.java
+++ src/test/java/com/uce/edu/Pa2U2P5AcApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Pa2U2P5AsApplicationTests {
+class Pa2U2P5AcApplicationTests {
 
 	@Test
 	void contextLoads() {
