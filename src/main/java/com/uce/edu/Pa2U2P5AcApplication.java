b40e075d396d97b4c55157898a3858d18d475f1b
Mauricio Cacuango <amcacuangoa@uce.edu.ec>
2024-01-02 19:54:52 -0500

    Taller 18

--- src/main/java/com/uce/edu/Pa2U2P5AcApplication.java
+++ src/main/java/com/uce/edu/Pa2U2P5AcApplication.java
@@ -1,26 +1,23 @@
 package com.uce.edu;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.repository.modelo.Ciudadano;
-import com.uce.edu.repository.modelo.Empleado;
-import com.uce.edu.service.ICiudadanoService;
-import com.uce.edu.service.IEmpleadoService;
+import com.uce.edu.repository.modelo.Habitacion;
+import com.uce.edu.repository.modelo.Hotel;
+import com.uce.edu.service.IHotelService;
 
 @SpringBootApplication
 public class Pa2U2P5AcApplication implements CommandLineRunner {
 	
 	@Autowired
-	private IEmpleadoService iEmpleadoService;
+	private IHotelService iHotelService;
 	
-	@Autowired
-	private ICiudadanoService iCiudadanoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U2P5AcApplication.class, args);
@@ -28,22 +25,29 @@ public class Pa2U2P5AcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+	
+		Hotel hotel = new Hotel();
+		hotel.setDireccion("La colon");
+		hotel.setNombre("Marriot");
+		//hotel.setHabitaciones(null);
 		
-		Ciudadano ciud = new Ciudadano();
-		ciud.setNombre("Mauricio");
-		ciud.setApellido("Cacuango");
+		Habitacion h1 = new Habitacion();
+		h1.setClase("Economica");
+		h1.setNumero("A1");
+		h1.setHotel(hotel);
 		
-		Empleado empl = new Empleado();
-		empl.setSalario(new BigDecimal(460));
-		empl.setFechaIngreso(LocalDateTime.now());
-		empl.setCiudadano(ciud);
+		Habitacion h2 = new Habitacion();
+		h2.setClase("Presidencial");
+		h2.setNumero("H2");
+		h2.setHotel(hotel);
 		
-		empl.setCiudadano(this.iCiudadanoService.buscar(1));
-		this.iEmpleadoService.agregar(empl);
-		//this.iCiudadanoService.agregar(ciud);
+		List<Habitacion> habitaciones = new ArrayList<>();
+		habitaciones.add(h1);
+		habitaciones.add(h2);
 		
-		//this.iCiudadanoService.buscar(1);
+		hotel.setHabitaciones(habitaciones);
 		
+		this.iHotelService.guardar(hotel);
 	}
 
 }
---
+++ src/main/java/com/uce/edu/repository/HotelRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.uce.edu.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.uce.edu.repository.modelo.Hotel;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
+public class HotelRepositoryImpl implements IHotelRepository {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Override
+	public void insertar(Hotel hotel) {
+		this.entityManager.persist(hotel);
+	}
+
+}
