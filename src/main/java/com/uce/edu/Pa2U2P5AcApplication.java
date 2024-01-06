8f63cedfabfe3bd77dd431baa499ec31bfe5ba75
Mauricio Cacuango <amcacuangoa@uce.edu.ec>
2024-01-05 19:51:43 -0500

    Taller 19

--- src/main/java/com/uce/edu/Pa2U2P5AcApplication.java
+++ src/main/java/com/uce/edu/Pa2U2P5AcApplication.java
@@ -1,22 +1,24 @@
 package com.uce.edu;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.repository.modelo.Habitacion;
-import com.uce.edu.repository.modelo.Hotel;
-import com.uce.edu.service.IHotelService;
+import com.uce.edu.repository.modelo.Autor;
+import com.uce.edu.repository.modelo.Libro;
+import com.uce.edu.service.ILibroService;
 
 @SpringBootApplication
 public class Pa2U2P5AcApplication implements CommandLineRunner {
 	
 	@Autowired
-	private IHotelService iHotelService;
+	private ILibroService iLibroService;
+	
 	
 
 	public static void main(String[] args) {
@@ -26,28 +28,32 @@ public class Pa2U2P5AcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 	
-		Hotel hotel = new Hotel();
-		hotel.setDireccion("La colon");
-		hotel.setNombre("Marriot");
-		//hotel.setHabitaciones(null);
-		
-		Habitacion h1 = new Habitacion();
-		h1.setClase("Economica");
-		h1.setNumero("A1");
-		h1.setHotel(hotel);
-		
-		Habitacion h2 = new Habitacion();
-		h2.setClase("Presidencial");
-		h2.setNumero("H2");
-		h2.setHotel(hotel);
-		
-		List<Habitacion> habitaciones = new ArrayList<>();
-		habitaciones.add(h1);
-		habitaciones.add(h2);
-		
-		hotel.setHabitaciones(habitaciones);
-		
-		this.iHotelService.guardar(hotel);
+	Libro lib = new Libro();
+	lib.setTitulo("Cien anios de soledad");
+	lib.setFechaPublicacion(LocalDateTime.now());
+	
+	Autor autor = new Autor();
+	autor.setNombre("Augusto Salazar");
+	autor.setNacionalidad("Venezolano");
+	
+	Autor autor2 = new Autor();
+	autor2.setNombre("Jhon Jairo");
+	autor2.setNacionalidad("Ecuatoriano");
+	
+	Set<Autor> autores = new HashSet<Autor>();
+	autores.add(autor);
+	autores.add(autor2);
+	
+	lib.setAutores(autores);
+	
+	Set<Libro> libros = new HashSet<>();
+	libros.add(lib);
+	
+	autor.setLibros(libros);
+	autor2.setLibros(libros);
+	
+	this.iLibroService.agregar(lib);
+	
 	}
 
 }
